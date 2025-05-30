3212dfc3be8a724dded3eac7768574c23335a0ec
Jedidiah Solomon <121108148+Jedidiah-Solomon@users.noreply.github.com>
2025-05-30 17:20:54 +0800

    Server created

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const morgan = require("morgan");
+require("dotenv").config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(morgan("dev"));
+
+app.get("/", (req, res) => {
+  res.send("Welcome...........Server Running!");
+});
+
+app.get("/life-stats", (req, res) => {
+  const lifeStats = {
+    name: "Creative Coder Jed",
+    coffeeCupsToday: 3,
+    linesOfCode: 1427,
+    gitCommits: 6,
+    debuggingHours: 1.5,
+    mood: " Focused & Shipping videos for developers",
+    inspiration:
+      "“Code is like humor. When you have to explain it, it’s bad.” – Cory House",
+  };
+  res.json(lifeStats);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
